0a876aa5d6fdc5ffb455d2ab0fdbc728932994c6
Wytamma Wirth <wytamma.wirth@me.com>
2025-08-05 12:21:35 +1000

    Add batch size configuration for DeepLabCut processing
    - Introduced `batch-size` parameter in `snk.yaml` to specify the number of frames processed in a single batch.
    - Updated `Snakefile` to utilize the new `batch-size` parameter.
    - Modified `run_deeplabcut.py` to accept `--batch-size` argument and pass it to the processing function.
    - Added a new PDF file `dag.pdf`.

--- src/roithepig/workflow/scripts/run_deeplabcut.py
+++ src/roithepig/workflow/scripts/run_deeplabcut.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     argparser.add_argument('--video', type=str, required=True, help='Path to the video file.')
     argparser.add_argument('--output-dir', type=str, required=True, help='Directory to save the output results.')   
     argparser.add_argument('--cropping', type=str, default=None, help='Optional cropping coordinates in the format "x1,x2,y1,y2". If not provided, no cropping will be applied.')
+    argparser.add_argument('--batch-size', type=int, default=2, help='Number of frames to process in a single batch.')
 
     args = argparser.parse_args()
 
@@ -17,7 +18,7 @@ if __name__ == "__main__":
         [args.video],
         save_as_csv=True, 
         destfolder=args.output_dir,
-        batch_size=2,
+        batch_size=args.batch_size,
         cropping=[int(i) for i in args.cropping.split(',')] if args.cropping else None
     )
     deeplabcut.filterpredictions(
